6e427850e8bf20be68e95a3c4b0a5c91c66f749d
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-02-19 21:53:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-	let i = 0 ;
+		let i = 0 ;
 	let n = str.length-1;
+	
+	   
 	while(i<n){
-	    if(str[i]!==str[n]) return false;
+	    if(str[i].toLowerCase()!==str[n].toLowerCase() ){
+	        return false;
+	        
+	    };
 		i++;
 		n--;
 	}
